e2f8f420b8c0d26f6512065cf8039dc608c3d912
pritesh <priteshbhole11@gmail.com>
2016-10-07 09:25:03 +0100

    cleanup code added filter

--- src/main/java/firsttry/booking/BookingController.java
+++ src/main/java/firsttry/booking/BookingController.java
@@ -1,13 +1,11 @@
 package firsttry.booking;
 
 
-import com.sun.org.apache.regexp.internal.RE;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by pritesh on 06/10/16.
@@ -27,5 +25,11 @@ public class BookingController {
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public List<HotelBooking> getAll(){
         return bookings;
+    }
+    @RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
+    public List<HotelBooking> getAffordable(@PathVariable double price){
+        return bookings.stream().filter(x -> x.getPricePerNight() <= price)
+                .collect(Collectors.toList());
+
     }
 }
